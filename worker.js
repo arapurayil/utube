6e44503eb008c92103b7e70794e27f10a3e7583c
arapurayil <51992491+arapurayil@users.noreply.github.com>
2021-09-08 00:19:27 +0530

    Update worker.js
    changed url path
    support for dash streams
    support for youtube live video id

--- worker.js
+++ worker.js
@@ -1,15 +1,56 @@
-addEventListener("fetch", (event) => {
-    event.respondWith(
-        handleRequest(event.request).catch(
-            (err) => new Response(err.stack, {status: 500})
-        )
-    );
-});
+async function dashUrl(ytUrl) {
+  return await fetch(ytUrl)
+    .then(async (r) => await r.text())
+    .then((r) => r.match(/(?<=dashManifestUrl":").+?(?=",)/g)[0]);
+}
+
+async function hlsUrl(ytUrl) {
+  return await fetch(ytUrl)
+    .then(async (r) => await r.text())
+    .then((r) => r.match(/(?<=hlsManifestUrl":").*\.m3u8/g)[0]);
+}
 
 async function handleRequest(request) {
-    let destinationURL = new URL(request.url).searchParams.get('url')
-    destinationURL = await fetch(destinationURL)
-    destinationURL = await destinationURL.text()
-    destinationURL = destinationURL.match(/(?<=hlsManifestUrl":").*\.m3u8/g)[0];
-    return Response.redirect(destinationURL, 302)
+  let pathName = new URL(request.url).pathname.split("/");
+  switch (pathName[3]) {
+    case "master.mpd":
+      try {
+        return Response.redirect(
+          await dashUrl(
+            "https://www.youtube.com/channel/" + pathName[2] + "/live"
+          ),
+          302
+        );
+      } catch (err) {
+        return Response.redirect(
+          await dashUrl("https://www.youtube.com/watch?v=" + pathName[2]),
+          302
+        );
+      }
+    case "master.m3u8":
+      try {
+        return Response.redirect(
+          await hlsUrl(
+            "https://www.youtube.com/channel/" + pathName[2] + "/live"
+          ),
+          302
+        );
+      } catch (err) {
+        return Response.redirect(
+          await hlsUrl("https://www.youtube.com/watch?v=" + pathName[2]),
+          302
+        );
+      }
+    default:
+      console.log("Error: Invalid path. Use master.mpd or master.m3u8");
+      break;
+  }
 }
+
+addEventListener("fetch", async (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
